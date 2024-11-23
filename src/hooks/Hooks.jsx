b6cc4d5fae10c617be004a2848400c81efa9ef2c
Muhammad Tamim <muhammad.tamim.workplace@gmail.com>
2024-11-23 18:35:37 +0600

    optimize some code for small file making from Hook.jsx

--- src/hooks/Hooks.jsx
+++ src/hooks/Hooks.jsx
@@ -16,13 +16,11 @@ const Hooks = ({ AreaSearch, NameSearch }) => {
         let filteredCollactions = brokers;
 
         // Filter by Name if NameSearch is provided
-        if ((NameSearch && AreaSearch || NameSearch | AreaSearch) !== '') {
-            filteredCollactions = filteredCollactions.filter(broker =>
-                broker.Name && typeof broker.Name === 'string' && broker.Name.toLowerCase().includes(NameSearch.toLowerCase())
-                &&
-                broker.Area_Name && typeof broker.Area_Name === 'string' && broker.Area_Name.toLowerCase().includes(AreaSearch.toLowerCase())
-            );
-        }
+        filteredCollactions = filteredCollactions.filter(broker =>
+            broker.Name && typeof broker.Name === 'string' && broker.Name.toLowerCase().includes(NameSearch.toLowerCase())
+            &&
+            broker.Area_Name && typeof broker.Area_Name === 'string' && broker.Area_Name.toLowerCase().includes(AreaSearch.toLowerCase())
+        );
 
         // Update the filterData state with the final filtered data
         setFilterData(filteredCollactions);
